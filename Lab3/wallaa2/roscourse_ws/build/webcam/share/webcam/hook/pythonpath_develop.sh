29d268878c32a01a56950c7269e8a4c3bb804f09
awallach4 <awallach4@gmail.com>
2026-02-13 00:56:28 -0500

    Added Alex's Lab3 prelab and first 2 tasks.

---
+++ Lab3/wallaa2/roscourse_ws/build/webcam/share/webcam/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/yahboom/Desktop/team_repo/Lab3/wallaa2/roscourse_ws/build/webcam"
