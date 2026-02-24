c851770f74c85962eca70845c189a45d155288a4
wojnae38 <wojnae38@gmail.com>
2026-02-25 02:45:43 +0800

    Upload individual folders and files

---
+++ Lab3/wojnae/roscourse_ws/build/python_turtle/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/yahboom/S26_RobotProgramming_Eloquent_Elusor/Lab3/wojnae/roscourse_ws/install/python_turtle'
---
+++ Lab3/wojnae/roscourse_ws/build/python_turtle/share/python_turtle/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/yahboom/S26_RobotProgramming_Eloquent_Elusor/Lab3/wojnae/roscourse_ws/build/python_turtle"
