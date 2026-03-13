a4a755626f95457dcb01ef41b6d5595fcd552359
AllenPaily <allen.paily7@gmail.com>
2026-03-14 01:19:48 +0800

    Lab4 folder

---
+++ Lab4/pailya/roscourse_ws/src/build/turtle_interfaces/turtle_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yahboom/Desktop/S26_RobotProgramming_Eloquent_Elusor/Lab3/pailya/roscourse_ws/src/build/turtle_interfaces/turtle_interfaces__py
+# Build directory: /home/yahboom/Desktop/S26_RobotProgramming_Eloquent_Elusor/Lab3/pailya/roscourse_ws/src/build/turtle_interfaces/turtle_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
