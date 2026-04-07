d149e70a1be323c5db806cbba8b953c92675cc64
wojnae38 <wojnae38@gmail.com>
2026-04-08 00:44:55 +0800

    Lab 6 Task 2: add RViz launch file and config

---
+++ Lab6/wojnae/roscourse_sim_ws/build/my_robot/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/yahboom/S26_RobotProgramming_Eloquent_Elusor/Lab6/wojnae/roscourse_sim_ws/install/my_robot'
--- Lab6/wojnae/roscourse_sim_ws/src/my_robot/setup.py
+++ Lab6/wojnae/roscourse_sim_ws/src/my_robot/setup.py
@@ -1,4 +1,7 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
+
 
 package_name = 'my_robot'
 
@@ -10,6 +13,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+	(os.path.join('share', package_name), glob('urdf/*')),
+	(os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+	(os.path.join('share', package_name), glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
