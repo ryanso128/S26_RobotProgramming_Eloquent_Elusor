3e92e09b007d61cc6f9b2b4d743de3d3fb074f9a
wojnae38 <wojnae38@gmail.com>
2026-04-17 23:09:16 +0800

    Lab 7 Webots prelab: robot driver, world, launch files

---
+++ Lab7/wojnae/webots_ws/build/webots_test/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/yahboom/S26_RobotProgramming_Eloquent_Elusor/Lab7/wojnae/webots_ws/install/webots_test'
---
+++ Lab7/wojnae/webots_ws/src/webots_test/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+package_name = 'webots_test'
+
+data_files = []
+data_files.append(
+    ('share/ament_index/resource_index/packages',
+     ['resource/' + package_name])
+)
+data_files.append(
+    ('share/' + package_name + '/launch',
+     ['launch/robot_launch.py'])
+)
+data_files.append(
+    ('share/' + package_name + '/worlds',
+     ['worlds/my_world.wbt'])
+)
+data_files.append(
+    ('share/' + package_name + '/urdf',
+     ['urdf/my_robot.urdf'])
+)
+data_files.append(
+    ('share/' + package_name,
+     ['package.xml'])
+)
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yahboom',
+    maintainer_email='wojnae38@gmail.com',
+    description='Webots ROS2 example package',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_robot_driver = webots_test.my_robot_driver:main',
+        ],
+    },
+)
