732044eadfa9390471127e13008caa4a676bff27
AllenPaily <154101388+AllenPaily@users.noreply.github.com>
2026-04-24 13:19:04 -0400

    Update setup.py

--- yahboomcar_ws/src/yahboomcar_mediapipe/setup.py
+++ yahboomcar_ws/src/yahboomcar_mediapipe/setup.py
@@ -34,6 +34,7 @@ setup(
         'RobotCtrl = yahboomcar_mediapipe.RobotCtrl:main',
         'control_shape = yahboomcar_mediapipe.control_shape:main',
         'FingerCtrl = yahboomcar_mediapipe.FingerCtrl:main'
+        'bag_of_tricks = yahboomcar_mediapipe.bag_of_tricks:main'
         ],
     },
 )
